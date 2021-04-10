bed194982c5a10fd1dd72e6df1ad678cd33b29ac
Alina Ryabtsev <alina.ryabtsev@gmail.com>
2021-04-11 00:36:36 +0300

    Added plotting no feedback trials analysis

--- STLearningAcrossConditions.py
+++ STLearningAcrossConditions.py
@@ -40,7 +40,7 @@ class ShortTermLearningAcrossConditions:
         for condition in constants.CONDITIONS:
             self.accuracy_over_time_condition_per_rank(condition)
 
-    def relative_accuracy_over_each_trial_in_condition_ranks(self):
+    def relative_accuracy_over_each_trial_in_condition_ranks(self, feedback=True):
         """
         Generates graphs per each condition, where it takes the average of success rate of a
         stimuli from the given condition against how many times the stimuli appeared.
@@ -50,14 +50,17 @@ class ShortTermLearningAcrossConditions:
 
         Output: One line for Rank 1 (average all stimuli of rank 1), one line for rank 2 (all
         stimuli of rank 2), one line for rank 3 (all stimuli)
+        :param feedback: plot the stimuli with feedback or those without
         """
         fig, axs = plt.subplots(nrows=4, figsize=(10, 30))
-        fig.suptitle(f"relative accuracy for possible conditions")
+        fig.suptitle(f"relative accuracy for possible conditions with"
+                     f"{'' if feedback else ' no'} feedback")
         for i, condition in enumerate(constants.CONDITIONS):
-            df = self.da.get_relative_accuracy_mean_per_condition_all_data(condition)
+            df = self.da.get_relative_accuracy_mean_per_condition_all_data(condition, feedback)
             ax = sns.lineplot(data=df, x=constants.APPEARANCES, y=constants.RELATIVE_ACCURACY,
                               hue=constants.RANK, ax=axs[i])
             ax.set_title(f"relative accuracy of condition {condition} over trials")
             ax.set(ylim=(0, 1))
             ax.set(xlim=(1, None))
-        plt.savefig(f"relative_accuracy_over_trials.pdf")
+        plt.savefig(f"relative_accuracy_over_trials{'' if feedback else '_no_feedback'}.pdf")
+
--- runner.py
+++ runner.py
@@ -5,7 +5,8 @@ import constants
 
 
 def main():
-    databases = [constants.PATH_TO_FILES + filename for filename in constants.DATABASES]
+    # databases = [constants.PATH_TO_FILES + filename for filename in constants.DATABASES]
+    databases = [constants.DATABASE_FILE_NAME]
     db = DataBaseManager.DataBaseManager(*databases)
     stl_analyser = ShortTermLearningAcrossConditions(db)
     # stl_analyser.accuracy_over_time_by_probabilities()
@@ -13,7 +14,7 @@ def main():
     # rt_analyser = ReactionTime(db)
     # rt_analyser.mean_reaction_time_correct_vs_incorrect_all_blocks()
     # rt_analyser.mean_reaction_time_correct_vs_incorrect_per_condition()
-    stl_analyser.relative_accuracy_over_each_trial_in_condition_ranks()
+    stl_analyser.relative_accuracy_over_each_trial_in_condition_ranks(False)
 
 
 if __name__ == '__main__':
